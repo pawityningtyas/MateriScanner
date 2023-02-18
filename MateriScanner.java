c84f4c1690fd64b8ae4c68b45c1b29c91a2d2c2c
pawityningtyas <pawityningtyas25@gmail.com>
2023-02-18 16:25:22 +0700

    MateriScanner

---
+++ MateriScanner.java
@@ -0,0 +1,14 @@
+package Github.MateriScanner;
+
+import java.util.Scanner;
+
+public class MateriScanner {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Nama Saya: ");
+        String nama = input.nextLine();
+        System.out.println("Nama: " + nama);
+
+    }
+
+}
